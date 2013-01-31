0258effceda61dd81ba10974963c86a25b0ee58c
Matthew Astley <mca@sanger.ac.uk>
2013-01-31 11:15:42 +0000

    fixes.
    honour umask on this end of rsync;
    magic strings into vars;
    cvs diffs likely to exit 1

--- fetch-SHARED.core.sh
+++ fetch-SHARED.core.sh
@@ -1,32 +1,37 @@
 #! /bin/sh
 
+SHARED=SHARED.core
+SHARED_DIFF=diff.$SHARED.log
 case "$1" in
     -n) DRY=-n ;;
     -y) DRY='' ;;
     *) echo "Syntax: $0 [ -y | -n ]
 
-Update ./SHARED.core/ from /nfs/WWWdev (wet or dry runs available)
+Update ./$SHARED/ from /nfs/WWWdev (wet or dry runs available)
 " >&2
         exit 1 ;;
 esac
 
 set -x
+set -e
 
 rsync $DRY --delete-excluded --exclude '*~' --exclude '.#*' --exclude '*.swp' \
-    -aiSWH /nfs/WWWdev/SHARED_docs/lib/core/ SHARED.core
+    -iSWH -rltgD -E /nfs/WWWdev/SHARED_docs/lib/core/ $SHARED
 
 if [ -z "$DRY" ]; then
-    {
-        cd SHARED.core
+    (
+        cd $SHARED
+        set +e
         cvs -qn up 2>&1
         cvs -q diff -u 2>&1
-    } > diff.SHARED.core.log+
-    mv diff.SHARED.core.log+ diff.SHARED.core.log
+        true
+    ) > $SHARED_DIFF+
+    mv  $SHARED_DIFF+ $SHARED_DIFF
 
-#    find SHARED.core -type d -name CVS -print0 | xargs -r0 rm -rf
+#    find $SHARED -type d -name CVS -print0 | xargs -r0 rm -rf
 # tidier, but will make churn on the rsync
 
-    git add -A SHARED.core nb.SHARED.core.diff
+    git add -A $SHARED $SHARED_DIFF
     git commit -m "$( basename $0 ): update"
 fi
 
