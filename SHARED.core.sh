bcf5a3ed93373bd9ae18817b9e18ed7f0846e8fd
Matthew Astley <mca@sanger.ac.uk>
2013-01-31 10:45:55 +0000

    record diffs during SHARED.core updater; don't need diffs on small branch

--- SHARED.core.sh
+++ SHARED.core.sh
@@ -16,6 +16,18 @@ rsync $DRY --delete-excluded --exclude '*~' --exclude '.#*' --exclude '*.swp' \
     -aiSWH /nfs/WWWdev/SHARED_docs/lib/core/ SHARED.core
 
 if [ -z "$DRY" ]; then
-    git add -A SHARED.core
+    {
+        cd SHARED.core
+        cvs -qn up 2>&1
+        cvs -q diff -u 2>&1
+    } > diff.SHARED.core.log+
+    mv diff.SHARED.core.log+ diff.SHARED.core.log
+
+#    find SHARED.core -type d -name CVS -print0 | xargs -r0 rm -rf
+# tidier, but will make churn on the rsync
+
+    git add -A SHARED.core nb.SHARED.core.diff
     git commit -m "$( basename $0 ): update"
 fi
+
+echo Done.
