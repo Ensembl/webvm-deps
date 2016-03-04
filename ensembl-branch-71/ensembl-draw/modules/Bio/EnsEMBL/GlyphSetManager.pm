ee836a392fa30c19e1099dabfda4b5ebec58313f
James Gilbert <jgrg@sanger.ac.uk>
2016-03-04 16:57:07 +0000

    Removed ensembl branches 70 to 75

--- ensembl-branch-71/ensembl-draw/modules/Bio/EnsEMBL/GlyphSetManager.pm
+++
@@ -1,7 +0,0 @@
-package Bio::EnsEMBL::GlyphSetManager;
-use strict;
-use base qw(Sanger::Graphics::GlyphSetManager);
-
-sub species_defs { return $_[0]->{'config'}->species_defs; }
-
-1;
--- ensembl-branch-73/ensembl-draw/modules/Bio/EnsEMBL/GlyphSet_feature.pm
+++
@@ -1,4 +0,0 @@
-package Bio::EnsEMBL::GlyphSet_feature;
-
-use strict;
-use base qw(Bio::EnsEMBL::GlyphSet);
